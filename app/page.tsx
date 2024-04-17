08e8eece577e0b95f06dc1976ac5e833b97af0ce
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-16 17:26:08 -0700

    poppins header font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'></main>
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className='space-y-6 text-center'>
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-lg",
+            poppins.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className='text-white text-lg'>
+          A simple identity verification service
+        </p>
+      </div>
+    </main>
   );
 }
