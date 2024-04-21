b040bab93e5afec747c9bc9ad61e64c638dcc80e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-20 20:11:11 -0700

    getuserbyemail

---
+++ data/user.ts
@@ -0,0 +1,28 @@
+import db from "@/lib/db";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    if (!email) return null;
+    const user = await db.user.findUnique({
+      where: { email },
+    });
+
+    return user;
+  } catch (error: any) {
+    console.error(error.message);
+    return null;
+  }
+};
+export const getUserById = async (id: string) => {
+  try {
+    if (!id) return null;
+    const user = await db.user.findUnique({
+      where: { id },
+    });
+
+    return user;
+  } catch (error: any) {
+    console.error(error.message);
+    return null;
+  }
+};
