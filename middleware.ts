40445c5d4c92f2dea83adb17281e5e27fe2a4fc7
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-22 18:18:33 -0700

    middleware config update

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,20 @@
-import { auth } from "./auth";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   // req.auth
+  const isLoggedIn = !!req.auth;
   console.log("test", req.nextUrl.pathname);
+  console.log({ isLoggedIn });
 });
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
   ],
 };
