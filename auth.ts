73d91bf8b0f5eba87c78b55e726f6d2ce01d3984
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-22 18:14:51 -0700

    auth jwt strategy

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "./lib/db";
-import GitHub from "next-auth/providers/github";
+import authConfig from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth(
   {
     adapter: PrismaAdapter(db),
-    providers: [GitHub],
+    session: { strategy: "jwt" },
+    ...authConfig,
   }
 );
