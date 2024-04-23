a10fbd7751f6b2b61afe9003d3876afc53bcea94
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-22 18:00:05 -0700

    middleware config

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "./auth";
+
+export default auth((req) => {
+  // req.auth
+  console.log("test", req.nextUrl.pathname);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
