7d1756f481c55d277eabf2cb24f6ad051d4db3e0
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-23 17:32:57 -0700

    redirect on guarded routes

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,42 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  authPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   // req.auth
-  const isLoggedIn = !!req.auth;
-  console.log("test", req.nextUrl.pathname);
-  console.log({ isLoggedIn });
+  const { nextUrl, auth } = req;
+
+  const isLoggedIn = !!auth;
+  const currentPath = nextUrl.pathname;
+
+  const isApiAuthRoute = currentPath.startsWith(authPrefix);
+  const isAuthRoute = authRoutes.includes(currentPath);
+  const isPublicRoute = publicRoutes.includes(currentPath);
+
+  if (isApiAuthRoute) return;
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)
+      );
+    } else {
+      return;
+    }
+  }
+
+  if (!isPublicRoute) {
+    return Response.redirect(
+      new URL("auth/login", nextUrl)
+    );
+  }
 });
 
 // Optionally, don't invoke Middleware on some paths
