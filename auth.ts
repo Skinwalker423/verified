792795d41cd6e9366995a5da14164c68a2ca5447
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-24 18:30:26 -0700

    auth jwt strategy

--- auth.ts
+++ auth.ts
@@ -5,6 +5,20 @@ import authConfig from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth(
   {
+    callbacks: {
+      jwt: async ({ token }) => {
+        return { token };
+      },
+      session: ({ session, token }) => {
+        console.log("session", session);
+        console.log({ token });
+        if (token.sub && session.user) {
+          session.user.id = token.sub;
+        }
+
+        return session;
+      },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
