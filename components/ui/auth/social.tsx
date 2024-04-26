214f8cb205bf0c7f065251ca29535eea58cade2e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-25 19:47:46 -0700

    onClick signin

--- components/ui/auth/social.tsx
+++ components/ui/auth/social.tsx
@@ -3,27 +3,29 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../button";
+import { signIn } from "next-auth/react";
 
 export const Social = () => {
+  const onClick = async (provider: "google" | "github") => {
+    await signIn(provider);
+  };
   return (
     <div className='flex items-center w-full gap-x-2'>
       <Button
         size={"lg"}
         variant={"outline"}
         className='w-full'
-        onClick={() => {
-          console.log("signing in with google");
-        }}
+        onClick={() => onClick("google")}
       >
         <FcGoogle />
       </Button>
+
       <Button
         size={"lg"}
         variant={"outline"}
         className='w-full'
-        onClick={() => {
-          console.log("signing in with github");
-        }}
+        type='submit'
+        onClick={() => onClick("github")}
       >
         <FaGithub />
       </Button>
