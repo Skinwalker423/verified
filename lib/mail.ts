44ad176593a9fef83152d9855e38d604aa7840d7
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-27 19:16:09 -0700

    resend sendVerificationEmail

---
+++ lib/mail.ts
@@ -0,0 +1,27 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (
+  email: string,
+  token: string
+) => {
+  const confirmLink = `${
+    process.env.NODE_ENV === "production"
+      ? process.env.BASE_URL
+      : "http://localhost:3000"
+  }/auth/new-verification?token=${token}`;
+
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Confirm your email",
+      html: `<p><a href="${confirmLink}">Click to confirm account</a></p>`,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+};
