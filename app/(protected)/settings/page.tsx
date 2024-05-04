d8101ac2b88e59e33d13bf2c25eaf6a5a9b2d069
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-03 19:30:51 -0700

    settings client session

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,29 +1,31 @@
 "use client";
 
+import { logout } from "@/actions/logout";
+import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { Suspense } from "react";
 
 const SettingsPage = () => {
-  // const session = await auth();
-  const { data } = useSession();
-
-  console.log({ data });
-
-  if (!data) return null;
+  const { data, status } = useSession();
+  if (status === "loading") return null;
 
   return (
     <div>
       SettingsPage
-      <h1>Hello, {data?.user?.name || "Stranger"}</h1>
-      <p>{data?.user?.email}</p>
-      {data?.user?.image && (
-        <Image
-          src={data?.user?.image}
-          alt='avatar'
-          width={24}
-          height={24}
-        />
-      )}
+      <Suspense fallback={"Loading..."}>
+        <h1>Hello, {data?.user?.name || "Stranger"}</h1>
+        <p>{data?.user?.email}</p>
+        {data?.user?.image && (
+          <Image
+            src={data?.user?.image}
+            alt='avatar'
+            width={24}
+            height={24}
+          />
+        )}
+        <Button onClick={() => logout()}>Sign Out</Button>
+      </Suspense>
     </div>
   );
 };
