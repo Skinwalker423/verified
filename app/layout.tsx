8ba5cd83582dec353601ca7bdd87059628a795c3
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-04 19:09:29 -0700

    removed session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SessionProvider>
-      <html lang='en'>
-        <body className={inter.className}>{children}</body>
-      </html>
-    </SessionProvider>
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
