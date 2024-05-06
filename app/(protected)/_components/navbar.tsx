8ba8c436f22873da7b4289dace90c1e205f02a49
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-05 19:19:56 -0700

    shad components

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UserButton } from "@/components/auth/user-button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -43,7 +44,7 @@ export const NavBar = () => {
           <Link href={"/settings"}>Settings</Link>
         </Button>
       </div>
-      <p>User Button</p>
+      <UserButton />
     </nav>
   );
 };
