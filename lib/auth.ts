7713b2f9f02d81983555965ed1a055a2f81ad4c5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-06 17:02:41 -0700

    getCurrentUser

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { auth } from "@/auth";
+
+export const getCurrentUser = async () => {
+  const session = await auth();
+
+  if (!session) return null;
+
+  return session.user;
+};
