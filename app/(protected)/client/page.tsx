b54aa23304ba7a821d548eaf912d076f0f68dc12
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-07 17:58:08 -0700

    client page

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import useCurrentUser from "@/hooks/use-current-user";
 import React from "react";
 
 const ClientPage = () => {
-  return <div>ClientPage</div>;
+  const { user } = useCurrentUser();
+  if (!user) return null;
+  return (
+    <div>
+      <h1>Client Page</h1>
+      <UserInfo label='Client Component' user={user} />
+    </div>
+  );
 };
 
 export default ClientPage;
