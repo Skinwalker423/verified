daf1b890bf3ee599a495d5883fc573ca99903bbc
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-08 18:12:13 -0700

    admin page

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+
+import { RoleGate } from "@/components/auth/role-gate";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+} from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <Card className='w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>
+          🔑 Admin
+        </p>
+      </CardHeader>
+      <CardContent className='space-y-4'>
+        <h1>Content for all</h1>
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          For admins only: Keep it going!
+        </RoleGate>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
