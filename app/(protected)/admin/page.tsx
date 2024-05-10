3a1b2078eea38e5c170a39285409dbef834ae8ba
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-09 20:50:53 -0700

    toast on success and error

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -8,6 +8,7 @@ import {
   CardContent,
 } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
 
 const AdminPage = () => {
   const handleAdminApi = async () => {
@@ -17,10 +18,31 @@ const AdminPage = () => {
 
     console.log("res", res);
 
-    if (res.ok) {
+    if (!res.ok) {
       console.log("is admin");
+      toast.success("Success!", {
+        style: {
+          background: "green",
+          color: "white",
+        },
+        description: "Admin api function successfully done",
+        action: {
+          label: "More info",
+          onClick: () =>
+            console.log("redirecting for more info"),
+        },
+      });
     } else {
       console.log("forbidden");
+      toast.error("Forbidden", {
+        style: {
+          background: "red",
+          color: "white",
+        },
+        description:
+          "You are not authorized to use this api",
+        closeButton: true,
+      });
     }
   };
 
@@ -36,7 +58,7 @@ const AdminPage = () => {
         <RoleGate allowedRole={UserRole.ADMIN}>
           For admins only: Keep it going!
         </RoleGate>
-        <div>
+        <div className='flex justify-between items-center w-full border border-slate-200 px-2 py-4 rounded-lg'>
           <h3>Admin only API route</h3>
           <Button onClick={handleAdminApi}>Test</Button>
         </div>
