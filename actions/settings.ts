3f50369ac50165ece1ef917a2cbd4a3446c98cae
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-10 17:36:52 -0700

    settings action update name

---
+++ actions/settings.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import * as z from "zod";
+
+import db from "@/lib/db";
+import { SettingsSchema } from "@/schemas";
+import { getUserById } from "@/data/user";
+import { getCurrentUser } from "@/lib/auth";
+
+export const settings = async (
+  values: z.infer<typeof SettingsSchema>
+) => {
+  const validatedFields = SettingsSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields" };
+  }
+
+  const user = await getCurrentUser();
+
+  if (!user?.id) return { error: "Unauthorized" };
+
+  const dbUser = await getUserById(user.id);
+
+  if (!dbUser) return { error: "Unauthorized" };
+
+  await db.user.updateMany({
+    where: { id: dbUser.id },
+    data: {
+      name: validatedFields.data.name,
+    },
+  });
+};
