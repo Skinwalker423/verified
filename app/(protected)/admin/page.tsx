ccff33a5d199bfcddc48c5491bc252ada04b7958
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-10 17:07:50 -0700

    error and success toasts

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { admin } from "@/actions/admin";
+import { FormError } from "@/components/auth/form-error";
+import { FormSuccess } from "@/components/auth/form-success";
 import { RoleGate } from "@/components/auth/role-gate";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardHeader,
   CardContent,
+  CardFooter,
 } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
 import { useState } from "react";
@@ -17,9 +20,36 @@ const AdminPage = () => {
   const [error, setError] = useState("");
 
   const handleAdminServerAction = async () => {
+    setError("");
+    setIsAdmin("");
     const { error, success } = await admin();
-    if (error) setError(error);
+    if (error) {
+      setError(error);
+      toast.error("Forbidden", {
+        style: {
+          background: "red",
+          color: "white",
+        },
+        description:
+          "You are not authorized to use this server action",
+        closeButton: true,
+      });
+
+      return;
+    }
     if (success) setIsAdmin(success);
+    toast.success("Success!", {
+      style: {
+        background: "green",
+        color: "white",
+      },
+      description: "Admin server action successfully done",
+      action: {
+        label: "More info",
+        onClick: () =>
+          console.log("redirecting for more info"),
+      },
+    });
   };
   const handleAdminApi = async () => {
     const res = await fetch(
@@ -72,7 +102,17 @@ const AdminPage = () => {
           <h3>Admin only API route</h3>
           <Button onClick={handleAdminApi}>Test</Button>
         </div>
+        <div className='flex justify-between items-center w-full border border-slate-200 px-2 py-4 rounded-lg'>
+          <h3>Admin only server action</h3>
+          <Button onClick={handleAdminServerAction}>
+            Test
+          </Button>
+        </div>
       </CardContent>
+      <CardFooter>
+        {isAdmin && <FormSuccess message={isAdmin} />}
+        {error && <FormError message={error} />}
+      </CardFooter>
     </Card>
   );
 };
