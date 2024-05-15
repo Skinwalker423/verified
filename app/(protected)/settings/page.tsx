7136cacfe70a9a27d85c35099b8f97c111413031
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-14 18:48:28 -0700

    onClick update name

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,14 +1,44 @@
 "use client";
 
 import { logout } from "@/actions/logout";
+import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
 
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import { useTransition } from "react";
+
 const SettingsPage = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const onClick = () => {
+    startTransition(() => {
+      settings({ name: "new name" }).then((data) => {
+        if (data?.error) {
+          console.error(data.error);
+        }
+      });
+    });
+  };
+
   return (
-    <div className='bg-white'>
-      SettingsPage
-      <Button onClick={() => logout()}>Sign Out</Button>
-    </div>
+    <Card className='w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>
+          ⚙️ Settings
+        </p>
+      </CardHeader>
+      <CardContent>
+        <Button disabled={isPending} onClick={onClick}>
+          Update Name
+        </Button>
+        <Button>Update Email</Button>
+      </CardContent>
+    </Card>
   );
 };
 
