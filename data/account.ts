69800a7914ec9cf2d84fa6bb71ff19bf9136a3e8
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-14 19:54:48 -0700

    getAccountByUserId

---
+++ data/account.ts
@@ -0,0 +1,18 @@
+import db from "@/lib/db";
+
+export const getAccountByUserId = async (
+  userId: string
+) => {
+  try {
+    const account = await db.account.findFirst({
+      where: { userId },
+    });
+
+    return account;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    return null;
+  }
+};
