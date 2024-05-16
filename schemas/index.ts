04fc2ecbdb829d664b08fc549410e2510918f9c5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-15 18:32:36 -0700

    added isTwoFactorEnabled, role, password, newPasswordfields to settings schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import { z } from "zod";
 
 export const LoginFormSchema = z.object({
@@ -57,12 +58,53 @@ export const NewPasswordFormSchema = z
     }
   );
 
-export const SettingsSchema = z.object({
-  name: z
-    .string()
-    .min(1, {
-      message: "Name must entered",
-    })
-    .optional(),
-  email: z.string().email().optional(),
-});
+export const SettingsSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, {
+        message: "Name must entered",
+      })
+      .optional(),
+    email: z.string().email().optional(),
+    isTwoFactorEnabled: z.boolean().optional(),
+    role: z
+      .enum([UserRole.ADMIN, UserRole.USER])
+      .optional(),
+    password: z
+      .string()
+      .min(6, {
+        message: "Password requires at least 6 characters",
+      })
+      .optional(),
+    newPassword: z
+      .string()
+      .min(8, {
+        message: "password required",
+      })
+      .optional(),
+  })
+  .refine(
+    ({ password, newPassword }) => password !== newPassword,
+    {
+      message: "New password cannot be the same as the old",
+      path: ["newPassword"],
+    }
+  )
+  .refine(
+    ({ password, newPassword }) => {
+      if (
+        (password && !newPassword) ||
+        (newPassword && !password)
+      ) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message:
+        "Both Old password and new password are required",
+      path: ["newPassword"],
+    }
+  );
