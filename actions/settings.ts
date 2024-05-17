6e78a6e1d50aa98ab945968c3e2ae9651c14abf3
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-16 19:17:44 -0700

    updated settings to send email verification on email change

--- actions/settings.ts
+++ actions/settings.ts
@@ -4,18 +4,14 @@ import * as z from "zod";
 
 import db from "@/lib/db";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { getCurrentUser } from "@/lib/auth";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const settings = async (
   values: z.infer<typeof SettingsSchema>
 ) => {
-  const validatedFields = SettingsSchema.safeParse(values);
-
-  if (!validatedFields.success) {
-    return { error: "Invalid fields" };
-  }
-
   const user = await getCurrentUser();
 
   if (!user?.id) return { error: "Unauthorized" };
@@ -24,10 +20,33 @@ export const settings = async (
 
   if (!dbUser) return { error: "Unauthorized" };
 
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.isTwoFactorEnabled = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "Email already in use" };
+    }
+
+    const verificationToken =
+      await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+  }
+
   await db.user.updateMany({
     where: { id: dbUser.id },
     data: {
-      name: validatedFields.data.name,
+      name: values.name,
     },
   });
 
